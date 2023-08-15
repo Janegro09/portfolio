ededa15626a645e316844d101079c329c975c59f
Javier <javier.sosa099@gmail.com>
2023-08-15 11:11:41 -0300

    corregimos sender

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -118,6 +118,7 @@ const ContactButton = styled.input`
   color: ${({ theme }) => theme.text_primary};
   font-size: 18px;
   font-weight: 600;
+  cursor: pointer;
 `
 
 
@@ -130,9 +131,11 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_t24daip', 'template_vso1rkb', form.current, 'elqKDzU9D5iWKzLEk')
       .then((result) => {
         setOpen(true);
+        console.log(result)
+        console.log("Enviado")
         form.current.reset();
       }, (error) => {
         console.log(error.text);
@@ -148,9 +151,9 @@ const Contact = () => {
         <Desc>Feel free to reach out to me for any questions or opportunities!</Desc>
         <ContactForm ref={form} onSubmit={handleSubmit}>
           <ContactTitle>Email Me 🚀</ContactTitle>
-          <ContactInput required placeholder="Your Email" name="from_email" />
-          <ContactInput required placeholder="Your Name" name="from_name" />
-          <ContactInput required placeholder="Subject" name="subject" />
+          <ContactInput required placeholder="Your Email" name="user_email" />
+          <ContactInput required placeholder="Your Name" name="user_name" />
+          <ContactInput required placeholder="Subject" name="user_subject" />
           <ContactInputMessage required placeholder="Message" rows="4" name="message" />
           <ContactButton type="submit" value="Send" />
         </ContactForm>
